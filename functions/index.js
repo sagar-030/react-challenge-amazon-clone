ce5fbf4f6e29773e4110cf9d4114ce217014db46
Sagar <96865822+sagar-030@users.noreply.github.com>
2022-07-20 17:42:31 +0530

    Update index.js

--- functions/index.js
+++ functions/index.js
@@ -2,7 +2,7 @@ const functions = require("firebase-functions");
 const express = require("express");
 const cors = require("cors");
 const stripe = require("stripe")(
-  "sk_test_51HPvU9DFg5koCdLGeOEiFvwHat4v8eMjX6SY0YCwxPBQBUPhKy1fPVhiSM5cQtgW7QBG9ydQcXnW57TDxVE2f3H000HSfmEQZF"
+  "sk_test_51LGyOdSBiSPupAB1TDL35u6xJsLgtp8RLElx8rVEoTlCS1tdNf0OCqys3sy5g82heEZH6zjWRGs9V4uSKswbTIvT00cD1ViBG7"
 );
 
 // API
