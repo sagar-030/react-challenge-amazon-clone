9cc06b2ca4baeeb9317dc81858fa89367833e1b8
Sagar <96865822+sagar-030@users.noreply.github.com>
2022-07-20 17:57:06 +0530

    Update axios.js

--- src/axios.js
+++ src/axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const instance = axios.create({
   // THE API (cloud function) URL
-  baseURL: 'https://us-central1-challenge-4b2b2.cloudfunctions.net/api'
+  baseURL: 'https://us-central1-clone-26c7e.cloudfunctions.net/api'
     // "http://localhost:5001/challenge-4b2b2/us-central1/api",
 });
 
