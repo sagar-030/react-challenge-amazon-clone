cbc5d809718b03bfac89dea14cbb220d85c79b03
Sagar <96865822+sagar-030@users.noreply.github.com>
2022-07-20 17:45:33 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,13 +1,13 @@
 import firebase from "firebase";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCcPSKlYtpdzBoAC8soeSmIARMzVKzrf5I",
-  authDomain: "challenge-4b2b2.firebaseapp.com",
-  databaseURL: "https://challenge-4b2b2.firebaseio.com",
-  projectId: "challenge-4b2b2",
-  storageBucket: "challenge-4b2b2.appspot.com",
-  messagingSenderId: "962418448875",
-  appId: "1:962418448875:web:f6cce5eeaf819481f661ae",
+ apiKey: "AIzaSyBiwE4nAlcz83G6Bumib3a5yD48oOz-8CI",
+    authDomain: "clone-26c7e.firebaseapp.com",
+    projectId: "clone-26c7e",
+    storageBucket: "clone-26c7e.appspot.com",
+    messagingSenderId: "986745354262",
+    appId: "1:986745354262:web:edae990c8c72c852009d37",
+    measurementId: "G-42XFNTKL0W"
 };
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
